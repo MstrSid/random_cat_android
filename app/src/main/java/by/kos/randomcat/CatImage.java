cf0eac66f6565ed6d624b6eec2534186f14efd1b
Eugene Kos <mstrsid@live.com>
2022-11-04 19:37:36 +0300

    done

---
+++ app/src/main/java/by/kos/randomcat/CatImage.java
@@ -0,0 +1,9 @@
+package by.kos.randomcat;
+
+public class CatImage {
+  private String url;
+
+  public String getUrl() {
+    return url;
+  }
+}
--- app/src/main/java/by/kos/randomcat/MainActivity.java
+++ app/src/main/java/by/kos/randomcat/MainActivity.java
@@ -1,13 +1,49 @@
 package by.kos.randomcat;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String BASE_URL = "https://cataas.com/cat?";
+  private static final String URL_OPTION_JSON = "json=true";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    loadCatImage();
+  }
+
+  private void loadCatImage() {
+    new Thread(() -> {
+      try {
+        URL url = new URL(BASE_URL + URL_OPTION_JSON);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = urlConnection.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuilder json = new StringBuilder();
+        String res;
+        do {
+          res = bufferedReader.readLine();
+          if (res != null) {
+            json.append(res);
+          }
+        } while (bufferedReader.readLine() != null);
+        Log.d("mainActivity", json.toString());
+
+      } catch (Exception e) {
+        Log.d("mainActivity", e.toString());
+      }
+    }).start();
   }
 }
